5d2cdbc9fb24f9830b1abad9f920db0ef47bd505
jatin <jatin123ekta456@gmail.com>
2022-07-15 23:26:29 +0530

    This commit includes the dependency for shimmer effect and shimmer effect shown before updating ui.

--- app/src/main/java/com/juno/junoandroidassignment/ui/fragments/FragmentHome.kt
+++ app/src/main/java/com/juno/junoandroidassignment/ui/fragments/FragmentHome.kt
@@ -1,5 +1,6 @@
 package com.juno.junoandroidassignment.ui.fragments
 
+import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.juno.junoandroidassignment.data.model.crypto.CryptoState
@@ -13,20 +14,27 @@ class FragmentHome: FragmentBase<FragmentHomeBinding>(FragmentHomeBinding::infla
     private val cryptoViewModel by activityViewModels<CryptoViewModel>()
     private val navArgs by navArgs<FragmentHomeArgs>()
 
-
     private fun setUpObserver() {
         cryptoViewModel.cryptoState.observe(viewLifecycleOwner) { response ->
             if (response.isNullOrEmpty()) {
 
             } else {
+                binding.layoutShimmer.stopShimmer()
+                binding.layoutShimmer.visibility = View.GONE
                 adapter.setItems(response as ArrayList<CryptoState>, navArgs.state)
             }
         }
         cryptoViewModel.error.observe(viewLifecycleOwner) { error ->
             if (error.error) {
-
+                binding.layoutShimmer.stopShimmer()
+                binding.layoutShimmer.visibility = View.GONE
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cryptoViewModel.cryptoState.value = null
     }
 
     override fun setUpUi() {
